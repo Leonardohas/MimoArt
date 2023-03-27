15518f63ea7c7c7d7720323f2b4124cc5a9f7d01
Leonardohas <leonardohas83@gmail.com>
2023-03-27 15:19:32 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <FirstComponent/>
+  </div>
+</template>
+
+<script>
+import FirstComponent from "./components/FirstComponent.vue"
+export default {
+  name: "App",
+  components: {
+    FirstComponent
+  }
+}
+</script>
