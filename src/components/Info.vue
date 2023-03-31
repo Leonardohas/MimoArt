fda908c095da9d619988a140497510edd375dde6
Leonardohas <leonardohas83@gmail.com>
2023-03-31 11:50:28 -0300

    learning about dynamic attributes

--- src/components/Info.vue
+++ src/components/Info.vue
@@ -10,18 +10,27 @@
                 <li>Python</li>
             </ul>
             <p v-show="showEmail" class="pt-3">Mande uma mensagem para: {{ email }}</p>
+            <p>Para acessar meu curriculum <a v-bind:href="link">clique aqui</a></p> <!-- Utilizar v-bind é o mesmo que utilizar : -->
+            <Picture />
+
         </v-col>
     </div>
 </template>
 
 <script>
+import Picture from './Picture.vue'
 export default {
     name: "Info",
+    components: {
+        Picture,
+    },
+
     data(){
         return {
             isWorking: true,
             showEmail: true,
             email: `leonardohas83@gmail.com`,
+            link: ``
         }
     }
 }
---
+++ src/components/Picture.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <img :src="mimoLogo" :alt="description">
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Picture",
+    data(){
+        return {
+            mimoLogo: "/img/mimiArtLogoSemFundo.png",
+            description: "Mimo logo"
+        }
+    }
+}
+</script>
