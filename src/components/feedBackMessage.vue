3d8e0c9f2f9484eb0c0ccd274ff52de88196f309
Leonardohas <leonardohas83@gmail.com>
2023-05-29 12:08:28 -0300

    Using css to personalize the feed back message

--- src/components/feedBackMessage.vue
+++ src/components/feedBackMessage.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div class="feedBackMessage">
         <p>{{ message }}</p>
     </div>
 </template>
@@ -12,3 +12,15 @@ export default {
     }
 }
 </script>
+
+<style scoped>
+    .feedBackMessage {
+        color: rgb(3, 133, 25);
+        background-color: rgb(170, 248, 170);
+        border: 2px solid rgb(30, 177, 108);
+        border-radius: 5px;
+        padding: 10px;
+        max-width: 400px;
+        margin: 30px auto
+    }
+</style>
