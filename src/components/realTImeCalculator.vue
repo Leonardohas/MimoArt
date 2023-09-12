7e8d19817615aa54a611682ded79e630ccb1f270
Leonardohas <leonardohas83@gmail.com>
2023-09-12 15:13:41 -0300

    improvements

--- src/components/realTImeCalculator.vue
+++ src/components/realTImeCalculator.vue
@@ -1,18 +1,27 @@
 <template>
     <div>
-        <v-container>
+        <v-container >
         <h1>Criando funções para mudar em tempo real</h1>
-        <v-card class="mt-6" title="Calculadora em tempo real" variant="tonal">
-            <v-text-field v-model="numero1" label="digite um numero" @input="somar"/>
-            <v-text-field v-model="numero2" label="digite um numero" @input="somar"/>
-            {{ resultado }}
-
-    
-            <!-- <input v-model="numero1" type="number" @input="somar">
-            +
-            <input v-model="numero2" type="number" @input="somar">
-            =
-            {{ resultado }} -->
+        <v-card class="mt-6" title="Calculadora em tempo real" variant="tonal" width="800">
+          <v-row class="ml-1">
+            <v-col cols="2">
+              <v-text-field v-model="firstNumber" v-on:keypress="blockLetters($event)"  label="First Number" @input="calculator()"/>
+            </v-col>
+            <v-col cols="2">
+              <v-select v-model="selectedOperator" :items="operators" />
+            </v-col>
+            <v-col cols="2">
+              <v-text-field v-model="secondNumber" label="Second Number" @input="calculator()"/>
+            </v-col>
+            <v-col align-self="center" cols="1">
+              <p>=</p>
+            </v-col>
+            <v-col align-self="center">
+              <h3>
+                {{ result }}
+              </h3>
+            </v-col>
+          </v-row>
         </v-card>
     </v-container>
     </div>
@@ -22,22 +31,87 @@
   export default {
     data() {
       return {
-        numero1: 0,
-        numero2: 0,
-        resultado: 0
+        firstNumber: 0,
+        secondNumber: 0,
+        result: 0,
+        selectedOperator: '+',
+        operators: ['+', '-', '÷', 'x'],
       };
     },
     methods: {
-      somar() {
-        // Converte os valores para números e calcula a soma
-        const valor1 = parseFloat(this.numero1);
-        const valor2 = parseFloat(this.numero2);
-        if (isNaN(valor1) || isNaN(valor2)) {
-            this.resultado = 0;
-        } else {
-            this.resultado = valor1 + valor2;
-        }
+      blockLetters(field) {
+      var letters = String.fromCharCode(field.keyCode);
+      if (/[A-Za-z]/.test(letters)) {
+        field.preventDefault();
+      } else {
+        return true
       }
+    },
+      
+      calculator() {
+      switch (this.selectedOperator) {
+      case '+':
+        this.sum();
+        break;
+      case '-':
+        this.subtraction();
+        break;
+      case '÷':
+        this.division();
+        break;
+      case 'x':
+        this.multiplication();
+        break;
+      default:
+        console.error('Operador inválido');
+        break;
+      }
+    },
+
+      sum() {
+        // Converte os valores para números e calcula a soma
+        const firstValue = parseFloat(this.firstNumber);
+        const secondValue = parseFloat(this.secondNumber);
+        if (isNaN(firstValue) || isNaN(secondValue)) {
+            this.result = 0;
+        } else {
+            this.result = firstValue + secondValue;
+            return this.result.toFixed(2);
+        }
+      },
+
+      subtraction(){
+        const firstValue = parseFloat(this.firstNumber);
+        const secondValue = parseFloat(this.secondNumber);
+        if (isNaN(firstValue) || isNaN(secondValue)) {
+            this.result = 0;
+        } else {
+            this.result = firstValue - secondValue;
+            return this.result.toFixed(2);
+        }
+      },
+
+      division(){
+        const firstValue = parseFloat(this.firstNumber);
+        const secondValue = parseFloat(this.secondNumber);
+        if (isNaN(firstValue) || isNaN(secondValue)) {
+            this.result = 0;
+          } else {
+            this.result = firstValue / secondValue;
+            return this.result.toFixed(2);
+        }
+      },
+
+      multiplication(){
+        const firstValue = parseFloat(this.firstNumber);
+        const secondValue = parseFloat(this.secondNumber);
+        if (isNaN(firstValue) || isNaN(secondValue)) {
+            this.result = 0;
+        } else {
+            this.result = firstValue * secondValue;
+            return this.result.toFixed(2);
+        }
+      },
     }
   };
   </script>
